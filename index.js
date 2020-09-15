0ba72781922f01cdf0650dd526106c590d57503c
Jacob Swetmore <jacobswetmore99@gmail.com>
2020-09-15 14:36:07 +0100

    ready for deployment

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const pool = require('./db');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+//process.env.PORT
+//process.env.NODE_ENV => production or undefined
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    //server static content
+    //npm run build
+    app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
+console.log(__dirname);
+console.log(path.join(__dirname, 'client/build'));
 
 //ROUTES
 
@@ -78,6 +92,10 @@ app.delete('/todos/:id', async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log('Server has started on port 5000');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+});
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`);
 });
